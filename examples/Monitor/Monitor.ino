569300d7c13b20e3e36ae69959d1bad3120f0e02
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-14 12:06:52 -0300

    :sparkles: feature (Monitor.ino): Now it has state logging logic, and a blank main function to write custom stuff

--- examples/Monitor/Monitor.ino
+++ examples/Monitor/Monitor.ino
@@ -1,19 +1,48 @@
 #include <PServo.h>
 
 #define __BAUD_RATE 115200
-#define __DELAY 1500
+#define __CLOCK_SPEED 750
+#define __BUTTON_PIN 9
 
-ps::PServo *servo_handler;
+unsigned long timer = 0;
+char logg[64];
+
+inline void display_header(void);
+inline void state_logger(char *const buffer, const ps::PServo *const pservo);
+inline void main_loop(void);
+
+ps::PServo *const my_servo = new ps::PServo(true);
 
 void setup(void) {
-  servo_handler = new ps::PServo();
+  pinMode(__BUTTON_PIN, OUTPUT);
 
   Serial.begin(__BAUD_RATE);
 }
 
 void loop(void) {
-  ps::State const curr_state = servo_handler->get_state();
+  bool const is_stoped = digitalRead(__BUTTON_PIN);
 
-  Serial.println((unsigned char)curr_state);
-  delay(__DELAY);
+  if (not is_stoped)
+    return;
+
+  state_logger(logg, my_servo);
+  Serial.println(logg);
+
+  main_loop();
+  delay(__CLOCK_SPEED);
+
+  ++timer;
+}
+
+inline void main_loop(void) {}
+
+inline void state_logger(char *const buff, const ps::PServo *const pservo) {
+  using namespace ps;
+
+  char const *s = state_text(pservo->get_state());
+  Props const p = pservo->get_props();
+
+  sprintf(buff, "[%.4d|%.4d] %3dº with %.4d, %2d/%d in %s",
+          (unsigned short)timer, (unsigned short)p.pc, p.pos, p.delay,
+          p.curr_action, p.actions_count, s);
 }
