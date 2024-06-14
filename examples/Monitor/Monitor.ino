df4c134326b42fbe4c186915792ca765bbf5f886
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-14 01:16:31 -0300

    :bug: fix (Monitor.ino): Now it uses the new PServo's API, which means that it compiles now

--- examples/Monitor/Monitor.ino
+++ examples/Monitor/Monitor.ino
@@ -3,16 +3,17 @@
 #define __BAUD_RATE 115200
 #define __DELAY 1500
 
-PServo::PServo *servo_handler;
+ps::PServo *servo_handler;
 
 void setup(void) {
-  servo_handler = new PServo::PServo();
+  servo_handler = new ps::PServo();
 
   Serial.begin(__BAUD_RATE);
 }
 
 void loop(void) {
-  Serial.println("Hello world!");
+  ps::State const curr_state = servo_handler->get_state();
 
+  Serial.println((unsigned char)curr_state);
   delay(__DELAY);
 }
