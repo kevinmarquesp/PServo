0e595daadecab3edf0ecd4d9b555fcb9cb5753cb
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-15 00:28:52 -0300

    :sparkles: feature: Created the core logic to update the _pos value each async delay

--- src/PServo.cpp
+++ src/PServo.cpp
@@ -1,17 +1,39 @@
 #include "PServo.h"
 
-ps::PServo *ps::PServo::begin(unsigned long const timer) {
+ps::PServo *ps::PServo::begin(void) {
   using namespace ps;
-
-  _timer = timer;
 
   switch (_state) {
   case State::STANDBY: // Initialize the machine action counter before run.
     _state = State::INITIALIZED;
     break;
 
+  case State::INITIALIZED: // Here, it will be ready to start the movements.
+    if (_actions_count < 1) {
+      _state = State::ERROR_NOMOVE;
+      break;
+    }
+
+    _reset_active_action_then_move();
+    break;
+
+  case State::DONE: // Reset or stop the machine if done, or goto next movement.
+    if (_active_action > _actions_count) {
+      if (_is_resetable)
+        _reset_active_action_then_move();
+      else
+        _state = State::HALTED;
+    }
+
+    break;
+
+  case State::PAUSED:
+    _pc = *_timer;
+    break;
+
   case State::HALTED:
-    _state = State::MOVING;
+    break;
+  case State::ERROR_NOMOVE:
     break;
 
   default:
@@ -20,6 +42,17 @@ ps::PServo *ps::PServo::begin(unsigned long const timer) {
 
   return this;
 }
+
+inline void ps::PServo::_reset_active_action_then_move(void) {
+  if (_actions_count < 1) {
+    _state = State::ERROR_NOMOVE;
+    return;
+  }
+
+  _state = State::IN_ACTION;
+  _active_action = 0;
+}
+
 ps::PServo *ps::PServo::move(unsigned char const next_pos,
                              unsigned short const delay) {
   using namespace ps;
@@ -29,12 +62,27 @@ ps::PServo *ps::PServo::move(unsigned char const next_pos,
     ++_actions_count;
     break;
 
-  case State::MOVING: // Start the async timer for the current action.
-                      // TODO!
+  case State::IN_ACTION: // Start the async timer for the current action.
+    if (_active_action != _curr_action)
+      break;
+
+    if (_pos == next_pos) {
+      _state = State::DONE;
+      break;
+    }
+
+    if (*_timer - _pc > delay) {
+      _pc = *_timer;
+      _pos = _pos < next_pos ? _pos + 1 : _pos - 1;
+    }
+
     break;
 
-  case State::DONE: // Pass to the next action number, then move again.
-                    // TODO!
+  case State::PAUSED:
+    break;
+  case State::HALTED:
+    break;
+  case State::ERROR_NOMOVE:
     break;
 
   default:
@@ -45,29 +93,9 @@ ps::PServo *ps::PServo::move(unsigned char const next_pos,
 }
 
 ps::PServo *ps::PServo::move(unsigned char const next_pos) {
-  return this->move(next_pos, _delay);
-}
-
-ps::PServo *ps::PServo::halt(void) {
   using namespace ps;
 
-  switch (_state) {
-  case State::INITIALIZED: // Stop the initialization.
-    _state = State::HALTED;
-    break;
-
-  case State::DONE: // Reset or stop the machine if it's in the last action.
-                    // TODO!
-    break;
-
-  case State::MOVING:
-    break;
-
-  default:
-    _state = State::ERROR_UNEXPECTED;
-  }
-
-  return this;
+  return this->move(next_pos, Default::DELAY);
 }
 
 ps::Props ps::PServo::get_props(void) {
@@ -75,13 +103,14 @@ ps::Props ps::PServo::get_props(void) {
 
   return Props{
       .pc = _pc,
+      .timer = _timer,
       .min = _min,
       .max = _max,
       .is_resetable = _is_resetable,
       .curr_action = _curr_action,
+      .active_action = _active_action,
       .actions_count = _actions_count,
       .pos = _pos,
-      .delay = _delay,
   };
 }
 
@@ -92,9 +121,9 @@ char const *ps::state_text(ps::State s) {
 
   return s == State::STANDBY       ? "STANDBY"
          : s == State::INITIALIZED ? "INITIALIZED"
-         : s == State::MOVING      ? "MOVING"
+         : s == State::IN_ACTION   ? "IN_ACTION"
          : s == State::WAITING     ? "WAITING"
          : s == State::DONE        ? "DONE"
          : s == State::PAUSED      ? "PAUSED"
-                                   : "UNKNOWN";
+                                   : "";
 }
