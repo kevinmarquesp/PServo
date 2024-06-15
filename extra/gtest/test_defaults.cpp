3c0002a7cb8305c42d694f20c4f18f32cdab43d6
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-15 10:07:16 -0300

    :white_check_mark: test (Defaults): Updated that test suit to use the new constructor and Props API

--- extra/gtest/test_defaults.cpp
+++ extra/gtest/test_defaults.cpp
@@ -5,7 +5,9 @@
 TEST(Defaults, should_return_default_constructor_props) {
   using namespace ps;
 
-  PServo pservo;
+  unsigned long timer = 0;
+
+  PServo pservo(&timer);
   State const s = pservo.get_state();
   Props const p = pservo.get_props();
 
@@ -17,15 +19,15 @@ TEST(Defaults, should_return_default_constructor_props) {
   EXPECT_EQ(p.curr_action, 0);
   EXPECT_EQ(p.actions_count, 0);
   EXPECT_EQ(p.pos, 0);
-  EXPECT_EQ(p.delay, Default::DELAY);
 }
 
 TEST(Defaults, should_asign_custom_restable) {
   using namespace ps;
 
+  unsigned long timer = 0;
   bool const is_resetable = true;
 
-  PServo pservo(is_resetable);
+  PServo pservo(&timer, is_resetable);
   Props const p = pservo.get_props();
 
   EXPECT_EQ(p.min, Default::MIN);
@@ -36,10 +38,11 @@ TEST(Defaults, should_asign_custom_restable) {
 TEST(Defaults, should_asign_custom_min_and_max) {
   using namespace ps;
 
+  unsigned long timer = 0;
   unsigned char const custom_min = 25;
   unsigned char const custom_max = 120;
 
-  PServo pservo(custom_min, custom_max);
+  PServo pservo(&timer, custom_min, custom_max);
   Props const p = pservo.get_props();
 
   EXPECT_EQ(p.min, custom_min);
@@ -50,11 +53,12 @@ TEST(Defaults, should_asign_custom_min_and_max) {
 TEST(Defaults, should_asign_custom_min_max_and_restable) {
   using namespace ps;
 
+  unsigned long timer = 0;
   unsigned char const custom_min = 5;
   unsigned char const custom_max = 185; // Numbers > 180 is also valid.
   bool const is_resetable = true;
 
-  PServo pservo(custom_min, custom_max, is_resetable);
+  PServo pservo(&timer, custom_min, custom_max, is_resetable);
   Props const p = pservo.get_props();
 
   EXPECT_EQ(p.min, custom_min);
